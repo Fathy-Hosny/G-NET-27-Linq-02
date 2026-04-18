0b0822faa42c1d2199c8c0b92f5ab596a77fb2a4
Fathy Hosny <ali01120939266@gmail.com>
2026-04-18 13:12:51 +0200

    Q1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LINQ
+﻿using LINQ.DataSources;
+
+namespace LINQ
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			
+			var products = Source.ProductList;
+			#region Q1
+
+			//var MostExpensiveProducts = products
+			//	.OrderByDescending(p => p.UnitPrice)
+			//	.Take(3);
+			//foreach (var product in MostExpensiveProducts)
+			//{
+			//	Console.WriteLine(product);
+			//}
+
+
+			#endregion
 		}
 	}
 }
